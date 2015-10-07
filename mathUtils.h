70a4f591dd6a7063e78f5add9e30c1aacf840cf7
16JaMill16 <jacobrmiller@comcast.net>
2015-10-06 21:38:36 -0500

    math stuff

---
+++ mathUtils.h
@@ -0,0 +1,10 @@
+//Addition
+int * addition(const int *numA, const int *numB, int sizeA, int sizeB, int *sizeOfResult);
+//Subtraction
+int * subtraction(const int *numA, const int *numB, int sizeA, int sizeB, int *sizeOfResult);
+//Multiplication
+int * multiplication(const int *numA, const int *numB, int sizeA, int sizeB, int *sizeOfResult);
+//Division
+int * division(const int *numA, const int *numB, int sizeA, int sizeB, int *sizeOfResult);
+//power
+int * power(const int *numA, const int *numB, int sizeA, int sizeB, int *sizeOfResult);
